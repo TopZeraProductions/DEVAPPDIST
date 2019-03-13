901b87bb1998526016299a03b85060acd530c9fb
Notim <paulino.joaovitor@yahoo.com.br>
2019-03-12 22:26:13 -0300

    Up

---
+++ 03-12-2019/consumoapi/aula5/02_climatempo_extra.py
@@ -0,0 +1,26 @@
+'''
+A API da climatempo está em https://advisor.climatempo.com.br
+
+Faça:
+
+1) Crie uma conta e consiga uma chave sua para acessar.
+
+2) Teste no Firefox um exemplo de temperatura atual
+(veja a documentação).
+
+3) Crie uma função temp_sao_paulo que retorna o temperatura
+atual em São Paulo.
+
+4) Crie uma função temp_fortaleza que retorna o temperatura
+atual em Fortaleza.
+
+5) Crie uma função pega_id que recebe o nome da cidade e
+retorna seu id.
+
+6) Desafio: Crie uma função pega_temp que recebe o nome da
+cidade e retorna a sua temperatura.
+'''
+
+import requests
+
+sua_chave = ''
