4a28d5d76dfd17320c6bc5a1116ba7a0403d8549
Notim <paulino.joaovitor@yahoo.com.br>
2019-05-14 21:40:37 -0300

    Up

--- AtividadesContinuas/AC06/SalaDeAulaApi/Cliente/Startup.py
+++ AtividadesContinuas/AC06/SalaDeAulaApi/Cliente/Startup.py
@@ -1,21 +1,42 @@
 import json
 import requests 
 
+
+def main():
+    while 1:
+        print("Ola seja bem vindo!!")
+        print("Digite a opçao desejada!!")
+        print("1 ==> cadastrar usuario")
+        print("2 ==> visualizar mensagens")
+        print("3 ==> enviar nova mensagem")
+        print("outro ==> sair")
+
+        option = int(input())
+
+        if option == 1:
+            cadastrar_usuario()
+        elif option == 2:
+            listar_mensagem()
+        elif option == 3:
+            listar_mensagem()
+        else:
+            break
+
+
 def listar_usuarios():
     r = requests.get("http://localhost:5080/usr")
     print(r.content)
 
+
 def cadastrar_usuario():
     usu = input("Digite o usuário: ")
-    data = {"nome": str(usu)}
-    js = json.dumps(data)
-    print(js)
+    data = json.dumps({"nome": str(usu)})
 
-    r = requests.post(url="http://localhost:5080/usr", data=js, headers={'Content-type': 'application/json', 'Accept': 'text/plain'}) 
-    print (r.content)
+    response = requests.post(url="http://localhost:5080/usr",
+                             data=data,
+                             headers={'Content-type': 'application/json', 'Accept': 'text/plain'})
+    print(response.content)
 
-cadastrar_usuario()
-exit(0)
 
 def listar_mensagem():
     r = requests.get("http://localhost:5080/msg")
@@ -30,10 +51,3 @@ def cadastrar_mensagem():
     data = {"de": int(de), "para": int(para), "segredo": str(segredo), "texto": str(texto)}
     js = json.dumps(data)
     print(js)
-
-
-r = requests.post(url="http://localhost:5080/msg", data=js, headers={'Content-type': 'application/json', 'Accept': 'text/plain'}) 
-print (r.content)
-
-
-cadastrar_mensagem()
